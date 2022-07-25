a57648e0618c8b7b8d418d7b4d4317cb2e40b579
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-07-25 09:17:35 -0300

    updated files in directory SRC/

--- src/routes/categoriesRoutes.ts
+++ src/routes/categoriesRoutes.ts
@@ -4,3 +4,15 @@ import { Router } from "express";
 const categoriesRoutes = Router();
 
 const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  const category = { name, description };
+
+  categories.push(category);
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
+import { categoriesRoutes } from "../src/routes/categoriesRoutes";
+
 const app = express();
 
 const port = 8888;
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World!" });
-});
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at http://127.0.0.1:${port}!`);
