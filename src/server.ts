c6780fc385eb9122e1b523824ae3b5d25d51b50f
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-07-25 07:53:47 -0300

    updated file SERVER.TS in project RENTALCARS

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 
 const app = express();
 
+const port = 8888;
+
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World!" });
 });
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(port, () => {
+    console.log(`Server is running at http://127.0.0.1:${port}!`);
+});
