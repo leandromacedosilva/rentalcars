1651df5d2e5b9ce386ea42f2c90986e35c7d7698
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-08-08 13:47:06 -0300

    updated files in SRC/REPOSITORIES/

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,39 +1,42 @@
 import { Category } from "../model/category";
 
-import { ICategoriesRepository, ICreateCategoryDTO, } from "./ICategoriesRepository";
+import {
+    ICategoriesRepository, 
+    ICreateCategoryDTO, 
+} from "./ICategoriesRepository";
 
 class CategoriesRepository implements ICategoriesRepository {
-  // private define como o atributo será acessado
-  private categories: Category[];
+    // private define como o atributo será acessado
+    private categories: Category[];
 
-  constructor() {
-    this.categories = [];
-  }
+    constructor() {
+        this.categories = [];
+    }
 
-  create({ description,  name }: ICreateCargoryDTO): void {
-    const category = new Category();
+    create({ description, name }: ICreateCategoryDTO): void {
+        const category = new Category();
   
-    Object.assign(category, {
-      name,
-      description,
-      created_at: new Date(),
-    });
+     Object.assign(category, {
+            name,
+            description,
+            created_at: new Date(),
+        });
   
-  this.categories.push(category);
-  }
+        this.categories.push(category);
+    }
 
-  // listar cadastros das categorias
-list(): Category[]{ 
-    return this.categories;
-}
-// Verificar cadastro
-findByName(name: string): Category{
-  const category = this.categories.find(
-    (category) => category.name === name
-  );
-  return category;
-}
-// fim checking cadastro
+    // listar cadastros das categorias
+    list(): Category[] { 
+        return this.categories;
+    }
+    // Verificar cadastro
+    findByName(name: string): Category {
+        const category = this.categories.find(
+            (category) => category.name === name
+        );
+        return category;
+    }
+    // fim checking cadastro
 }
 
 export { CategoriesRepository };
--- src/repositories/ICategoriesRepository.ts
+++ src/repositories/ICategoriesRepository.ts
@@ -1,15 +1,15 @@
 import { Category } from "../model/category";
 
 // DTO - Data Transfer Object
-interface ICreateCargoryDTO{
-  name: string;
-  description: string;
+interface ICreateCargoryDTO {
+    name: string;
+    description: string;
 }
 
 interface ICategoriesRepository {
-  findByName(name: string): Category;
-  list(): Category[];
-  create({name, description}: ICreateCargoryDTO): void;
+    findByName(name: string): Category;
+    list(): Category[];
+    create({ name, description }: ICreateCargoryDTO): void;
 }
 
 export { ICreateCargoryDTO, ICategoriesRepository };
