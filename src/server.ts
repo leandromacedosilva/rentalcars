226a5b8fe55797598f07f0521c976e2dde6fbc1e
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-08-21 21:21:33 -0300

    updated file server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categoriesRoutes";
+import { specificationRoutes } from "./routes/specificationRoutes";
 
 const app = express();
 
@@ -8,7 +9,8 @@ const port = 8888;
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running at http://127.0.0.1:${port}!`);
