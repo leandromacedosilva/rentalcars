485f17e2aa177b6fe7d5f08a2d786dd0ae9a2a63
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-08-28 23:25:33 -0300

    updated files in project RentalCars

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,14 @@ import express from "express";
 
 import { router } from "./routes";
 
+const port = 5000;
 // import { categoriesRoutes } from "./routes/categories.routes";
 // import { specificationsRoutes } from "./routes/specification.routes";
-
 const app = express();
-
 app.use(express.json());
 app.use(router);
 // app.use("/categories", categoriesRoutes);
 // app.use("/specifications", specificationsRoutes);
-
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(port, () => {
+    console.log(`Express server running at http://127.0.0.1:${port}`);
+});
