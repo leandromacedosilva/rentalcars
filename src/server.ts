68bd13c41e4a44595876ac6f7f1a6d47df6036c2
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-08-28 22:25:40 -0300

    updated files in project

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationsRoutes } from "./routes/specification.routes";
+import { router } from "./routes";
+
+//import { categoriesRoutes } from "./routes/categories.routes";
+//import { specificationsRoutes } from "./routes/specification.routes";
 
 const app = express();
 
 const port = 8888;
 
 app.use(express.json());
+app.use(router);
 
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+//app.use("/categories", categoriesRoutes);
+//app.use("/specifications", specificationsRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://127.0.0.1:${port}!`);
+    console.log(`Express server is running at http://127.0.0.1:${port}!`);
 });
