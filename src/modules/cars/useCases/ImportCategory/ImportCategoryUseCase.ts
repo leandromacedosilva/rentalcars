2d58dea1c4ff4aecd3875b0acac56bf099cd0ca4
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-08-29 21:32:53 -0300

    added directory/files in project RentalCars

---
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,8 @@
+// Create CLASS ImportCategoryUseCase
+class ImportCategoryUseCase {
+    execute(file: Express.Multer.File) {
+        console.log(file);
+    }
+}
+
+export { ImportCategoryUseCase };
